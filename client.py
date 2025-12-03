c4ce3604fe0c5c254afa361a71ff6f4d702fbb8d
NinIcaty <136358167+NinIcaty@users.noreply.github.com>
2025-12-03 19:13:58 +0530

    Update 1.1
    Added Quit command and password capability

--- client.py
+++ client.py
@@ -1,29 +1,46 @@
 import socket
 import threading
 
-SERVER = "YOUR.SERVER.IP"
+SERVER = "demo.arul.net.in" 
 PORT = 5000
+PASSWORD = "test"  
 
 def listen(sock):
     while True:
         try:
             msg = sock.recv(1024).decode()
             if not msg:
+                print("Disconnected from server.")
                 break
-            print(msg)
-        except:
-            print("Disconnected from server.")
+            
+            print(msg)  # Print server message
+
+            # Automatically send password if server asks for it
+            if "PasswordProtect" in msg:
+                print("THIS SERVER IS PASSWORD PROTECTED, SENDING PASSWORD")
+                sock.send(PASSWORD.encode())
+
+        except Exception as e:
+            print("Error:", e)
             break
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((SERVER, PORT))
 
+    # Start listening thread
     threading.Thread(target=listen, args=(sock,), daemon=True).start()
 
     while True:
-        message = input("")
-        sock.send(message.encode())
+        try:
+            message = input()
+            if message.lower() == "//:quit":
+                print("Exiting...")
+                break
+            sock.send(message.encode())
+        except Exception as e:
+            print("Error:", e)
+            break
 
 if __name__ == "__main__":
     main()
